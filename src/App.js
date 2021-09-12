834d96f208dbd8ca6bb420bcf8447ed6fdc42dca
troyLCC <timothyroy2011@gmail.com>
2021-09-12 13:44:59 +0600

    this is not working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 
 import './App.css';
-import React,{useRef} from 'react'
+import React,{useRef, useState} from 'react'
 function App() {
-  const countryInput = useRef(null)
+  const [data, setData] = useState([])
+  const countryInput = useRef('bangladesh')
   const fetchData = (countryName) => {
     fetch(`https://api.covid19api.com/dayone/country/${countryName}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data =>  {
+      setData(data)
+    })
   }
   return (
     <div>
+      {countryInput &&  <div>{countryInput.current.value}</div>}
+      
         <input type="text" ref={countryInput} />
         <button onClick={fetchData(countryInput.current.value)}>Fetch Data</button>
+        {data.map((d) => (
+           <div> 
+            <div>Date: {d.Date}</div>
+            <div>Deaths: {d.Deaths}</div>
+            <div>Confirmed Cases {d.Confirmed}</div>
+          </div>
+
+        ))}
     </div>
   );
 }
