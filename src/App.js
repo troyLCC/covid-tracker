818108c9415945813f3738600af0af1fab249c50
troyLCC <timothyroy2011@gmail.com>
2021-09-12 12:29:57 +0600

    fetching api test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React,{useRef} from 'react'
 function App() {
+  const countryInput = useRef(null)
+  const fetchData = (countryName) => {
+    fetch(`https://api.covid19api.com/dayone/country/${countryName}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <input type="text" ref={countryInput} />
+        <button onClick={fetchData(countryInput.current.value)}>Fetch Data</button>
     </div>
   );
 }
